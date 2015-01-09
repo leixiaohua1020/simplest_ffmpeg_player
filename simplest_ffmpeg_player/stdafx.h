3954e7533137b089e9b2595e229ba7290dc0d0c9
unknown <leixiaohua1020@126.com>
2015-01-09 15:51:19 +0800

    remove stdafx.h

--- simplest_ffmpeg_player/stdafx.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include "targetver.h"
-#include <stdio.h>
-#include <tchar.h>
-#include <io.h>
-#include <direct.h>
--- simplest_ffmpeg_player_su/stdafx.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-#include "targetver.h"
-
-#include <stdio.h>
-#include <tchar.h>
