88db9284a716c56af45e74f38a643d1ac97afab2
unknown <leixiaohua1020@126.com>
2015-02-01 13:17:43 +0800

    fix

--- simplest_ffmpeg_decoder_pure/compile_cl.bat
+++ simplest_ffmpeg_decoder_pure/compile_cl.bat
@@ -10,8 +10,9 @@
 ::VS2010 Environment
 call "D:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 ::include
-@set INCLUDE=include;%INCLUDE%;
+@set INCLUDE=include;%INCLUDE%
 ::lib
-@set LIB=lib;%LIB%;
+@set LIB=lib;%LIB%
 ::compile and link
 cl simplest_ffmpeg_decoder_pure.cpp /link avcodec.lib avutil.lib swscale.lib /OPT:NOREF
+exit
--- simplest_ffmpeg_player/compile_cl.bat
+++ simplest_ffmpeg_player/compile_cl.bat
@@ -10,10 +10,11 @@
 ::VS2010 Environment
 call "D:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 ::include
-@set INCLUDE=include;%INCLUDE%;
+@set INCLUDE=include;%INCLUDE%
 ::lib
-@set LIB=lib;%LIB%;
+@set LIB=lib;%LIB%
 ::compile and link
 cl simplest_ffmpeg_player.cpp /link SDL2.lib SDL2main.lib avcodec.lib ^
 avformat.lib avutil.lib avdevice.lib avfilter.lib postproc.lib swresample.lib swscale.lib ^
 /NODEFAULTLIB:LIBCMT.lib /OPT:NOREF
+exit
